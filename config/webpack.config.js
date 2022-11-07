d47627f0e68245b87af554f4988ef16373801d3b
sjy <1286847869@qq.com>
2022-11-07 15:54:43 +0800

    mod nginx publicPath

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -296,7 +296,7 @@ module.exports = function (webpackEnv) {
       // It requires a trailing slash, or the file assets will get an incorrect path.
       // We inferred the "public path" (such as / or /my-project) from homepage.
       // publicPath: paths.publicUrlOrPath,
-      publicPath: isEnvDevelopment ? '/': '../',
+      publicPath: isEnvDevelopment ? '/': '/reactEntry/',
       // Point sourcemap entries to original disk location (format as URL on Windows)
       devtoolModuleFilenameTemplate: isEnvProduction
         ? info =>
